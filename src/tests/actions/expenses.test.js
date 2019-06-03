eb182dda614c74f890216f7c0137e1945cbe06a3
iddle_254 <charlesiddle99@gmail.com>
2019-06-03 05:58:12 +0300

    setup action tests

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,50 @@
+import { addExpense, editExpense, removeExpense} from '../../actions/expenses';
+
+test(
+    'should setup remove expense action item', ()=>{
+        const result = removeExpense({id:'123abc'});
+        expect(result).toEqual({
+            type:'REMOVE_EXPENSE',
+            id:'123abc'
+        })
+    }
+);
+
+test(
+    'should setup edit expense action item', ()=>{
+        const result = editExpense('123abc',{note:'new note'});
+        expect(result).toEqual({
+            type:'EDIT_EXPENSE',
+            id:'123abc',
+            updates:{
+                note:'new note'
+            }
+            
+        });
+    }
+);
+
+test(
+    'should setup add expense action object with provided values',()=>{
+        const expenseData = {
+            description:'Rent',
+            amount:109500,
+            createdAt:1000,
+            note:'last months rent'
+        };
+        const result = addExpense(expenseData);
+        expect(result).toEqual({
+            type:'ADD_EXPENSE',
+            expense:{
+                ...expenseData,
+                id:expect.any(String)
+            }
+        })
+    }
+);
+
+test(
+    'should setup add expense action object with default values',()=>{
+        
+    }
+);
