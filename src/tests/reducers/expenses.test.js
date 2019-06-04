128d4086a31b062bca35c3b59205e91dd44a3bbb
iddle_254 <charlesiddle99@gmail.com>
2019-06-04 12:39:02 +0300

    ch-completed test on remove expense action component

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -6,3 +6,14 @@ test(
         expect(state).toEqual([]);
     }
 );
+
+test(
+    'should remove expenses by id',()=>{
+        const result = {
+            type:'REMOVE_EXPENSE',
+            id:expenses[1].id
+        }
+        const state = expensesReducer(expenses,result);
+        expect(state).toEqual([expenses[0],expenses[2]]);
+    }
+);
