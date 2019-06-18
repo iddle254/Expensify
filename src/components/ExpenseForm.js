39bff48684ebefecf536e4c9c4c5fd8377a39cd8
iddle_254 <charlesiddle99@gmail.com>
2019-06-19 02:40:44 +0300

    final deploy

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -54,20 +54,21 @@ export default class ExpenseForm extends React.Component {
     }
   };
   render() {
-    return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+    return (        
+        <form className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
           <input
             type="text"
             placeholder="Description"
             autoFocus
+            className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
             type="text"
             placeholder="Amount"
+            className="text-input"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
@@ -83,11 +84,14 @@ export default class ExpenseForm extends React.Component {
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
+            className="textArea"
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+          <button className="button ">Save Expense</button>
+          </div>
+          
         </form>
-      </div>
     )
   }
 }
